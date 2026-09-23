02f2c8e86e979dbd1f92e1006520aaa9f400e857
69011817 วชิระ <69011817@kmitl.ac.th>
2026-09-23 18:09:22 +0700

    Add farewell XD

--- hello.c
+++ hello.c
@@ -2,11 +2,18 @@
 int main(void)
 {
 greeting("Developer A,B,C !!\n");
+
+farewell("Developer A,B,C");
 return 0;
 }
 //Print a greeting message for specified user
 int greeting(const char *name)
 {
 printf("Hello, %s!\n", name);
+return 0;
+}
+int farewell(const char *name)
+{
+printf("Goodbye, %s!\n", name);
 return 0;
 }
