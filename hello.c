c84c001f9e328a6e3ca6eba7dfcbbc82b1871fa6
Nutpicha <Nutpicha555@hotmail.com>
2026-09-23 17:37:42 +0700

    Added a comment bro

--- hello.c
+++ hello.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main(void)
 {
-printf("Hello, Git Collaboration!\n");
+printf("Developer A,B,C !!\n");
 return 0;
 }
-
+//Print a greeting message for specified user
 int greeting(const char *name)
 {
 printf("Hello, %s!\n", name);
